fb9aa509a6142387943381c29739142bf327d36d
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-05-12 20:58:24 +0100

    test: add all check ins controllers e2e tests

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
 import { usersRoutes } from './http/controllers/users/_routes'
 import { gymRoutes } from './http/controllers/gyms/_routes'
+import { checkInsRoutes } from './http/controllers/check-ins/_routes'
 
 export const app = fastify()
 
@@ -13,6 +14,7 @@ app.register(fastifyJwt, {
 
 app.register(usersRoutes)
 app.register(gymRoutes)
+app.register(checkInsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
--- src/http/controllers/check-ins/metrics.ts
+++ src/http/controllers/check-ins/metrics.ts
@@ -2,7 +2,7 @@ import type { FastifyInstance } from 'fastify'
 import { makeGetUserMetricsService } from '@/services/factories/make-get-user-metrics-service'
 
 export async function metrics(app: FastifyInstance) {
-  app.get('/gyms/metrics', async (request, reply) => {
+  app.get('/check-ins/metrics', async (request, reply) => {
     const getUserMetricsService = makeGetUserMetricsService()
 
     const { checkInsCount } = await getUserMetricsService.execute({
